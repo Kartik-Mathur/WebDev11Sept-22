6cc73c54bab446d62f9694c8c3cfa8bfc0788569
kartik mathur <kartik.mathur10@gmail.com>
2022-10-16 12:52:22 +0530

    JavaScript

--- Lecture-10(JS-3)/5.ClosureAdvanced.js
+++ Lecture-10(JS-3)/5.ClosureAdvanced.js
@@ -14,8 +14,8 @@ let FunGen1 = outerFun();
 let fun1 = FunGen1();
 let fun2 = FunGen1();
 
-console.log(fun1());
-console.log(fun1());
-console.log(fun1());
-console.log(fun2());
-console.log(fun2());
+console.log(fun1()); // 4 3 3 3 4
+console.log(fun1()); // 5 5 4 4 5
+console.log(fun1()); // 6 7 5 5 6
+console.log(fun2()); // 4 3 6 4 7
+console.log(fun2()); // 5 5 7 5 8
