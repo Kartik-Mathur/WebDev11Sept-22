b651d4e86abd6d6c943d0d43c3920c4874850e13
kartik mathur <kartik.mathur10@gmail.com>
2022-11-19 12:27:52 +0530

    Tambola Basics

--- Lecture-18(Events-AJAX)/2.Tambola/script.js
+++ Lecture-18(Events-AJAX)/2.Tambola/script.js
@@ -1,9 +1,37 @@
 const container = document.querySelector('.container');
-
-for(let i = 1; i <= 100 ; i++){
+const btn = document.querySelector('#btn');
+let arr = [];
+for (let i = 1; i <= 10; i++) {
     let div = document.createElement('div');
+    arr.push(i);
     div.innerText = i;
-    div.setAttribute('id',i);
+    div.setAttribute('id', 'item-' + i);
     div.classList.add('div');
     container.appendChild(div);
 }
+// button click hone par ek number array se hatt jaaye,
+// aur uska color change ho jaaye screen par
+btn.addEventListener('click', (ev) => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    console.log(randomIndex);
+    // Random index wala element array se hatt jaaye
+    // aur is index ke element ka color page par change kro
+    let itemId;
+    if (arr.length > 0) {
+        arr = arr.filter((v, indx) => {
+            if (indx == randomIndex) {
+                itemId = arr[randomIndex];
+                let item = document.querySelector('#item-' + itemId);
+                console.log(item);
+                item.classList.add('changeColor');
+                return false;
+            }
+            return true;
+        })
+        // console.log(arr);
+    }
+    else{
+        container.innerText = 'GAME OVER';
+        btn.classList.add('gayab');
+    }
+})
